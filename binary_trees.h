8083017d451b3d0882438c5967e48f6ddd6364d3
george-ashraf-88 <georgeashraf88@icloud.com>
2024-06-22 12:32:21 +0300

    ff

--- binary_trees.h
+++ binary_trees.h
@@ -9,14 +9,14 @@
 /**
  * struct binary_tree_s - Binary tree node
  *
- * @v: Integer stored in the node
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
 struct binary_tree_s
 {
-    int v;
+    int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
