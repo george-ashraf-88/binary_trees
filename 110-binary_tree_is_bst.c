3c5859cf3312c92c313aec4ee366dc9f682866eb
george-ashraf-88 <georgeashraf88@icloud.com>
2024-07-01 15:35:39 +0300

    gf

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,37 +1,52 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
- *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
+ * find_node -function find node in a tree
+ * @root: root of the tre to evaluate
+ * @node: node to find
+ * Return: 1 if exits 0 if no
+ */
+int find_node(binary_tree_t *root, binary_tree_t *node)
+{
+
+	if (root == NULL)
+		return (0);
+	if (node == root)
+		return (1);
+	if (node->n < root->n)
+		return (find_node(root->left, node));
+	if (node->n > root->n)
+		return (find_node(root->right, node));
+	return (0);
+}
+/**
+ * croos_tree - cross the tree checking if each node exist correctly
+ * @root: root node of the tree
+ * @node: current node to evaluate
+ * Return: 1 if is BST0 if no
+ */
+int croos_tree(binary_tree_t *root, binary_tree_t *node)
+{
+	if (root && node)
+	{
+		int aux = 0;
+
+		aux = find_node(root, node);
+		if (node->left)
+			aux &= croos_tree(root, node->left);
+		if (node->right)
+			aux &= croos_tree(root, node->right);
+		return (aux);
+	}
+	return (0);
+}
+/**
+ * binary_tree_is_bst - check if ist a correctly bst tree
+ * @tree: tre to check
+ * Return: 1 if is bst 0 if not
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	return (btib_helper(tree, INT_MIN, INT_MAX));
-}
-
-/**
- * btib_helper - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
- * @min: Lower bound of checked nored
- * @max: Upper bound of checked nodes
- *
- * Return: 1 if tree is a valid BST
- *         0 otherwise
- */
-int btib_helper(const binary_tree_t *tree, int min, int max)
-{
-	if (!tree)
-		return (1);
-
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	return (btib_helper(tree->left, min, tree->n - 1) &&
-		btib_helper(tree->right, tree->n + 1, max));
-	/* -1 and +1 stem from "There must be no duplicate values" req */
+	return (croos_tree((binary_tree_t *)tree, (binary_tree_t *)tree));
 }
